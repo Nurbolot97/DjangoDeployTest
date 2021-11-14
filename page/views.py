4851366e4ae97b85a098a38f922d58e6b7495dad
Nurba <ndaiyrkanov@gmail.com>
2021-11-15 01:22:46 +0300

    Add hosts

--- page/views.py
+++ page/views.py
@@ -5,8 +5,9 @@ from .forms import PageForm
 
 def main(request):
     if request.method == "POST":
-        form = PageForm(request.POST, request.FILES)
+        form = PageForm(request.POST)
         if form.is_valid():
+            datas = form.cleaned_data['email']
             form.save()
             return redirect('result')
     else:
