e8f15f9d65c73d1ff14df88ab28a7a889888216e
Nurba <ndaiyrkanov@gmail.com>
2021-11-15 01:35:58 +0300

    Add hosts

--- page/views.py
+++ page/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, redirect
 from .forms import PageForm
+from .models import Data
 
 
 
 def main(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = PageForm(request.POST)
-        form.save()
-        return redirect('result')
-    else:
-        form = PageForm()
+        if form.is_valid():
+            datas = form.cleaned_data['login']
+            existing_login = Data.objects.filter(login=datas).count()   
+            if existing_login == 0:
+                form.save()
+            else:
+                form = PageForm()
     return render(request, 'main.html', locals())
 
 
